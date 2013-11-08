9b09da86bdc8fedd1942c8a86ee007ddb940572c
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-08 16:42:37 +0000

    pull in screenshots

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,22 @@ var AWS = require('aws-sdk'),
     s3  = new AWS.S3();
 
 exports.index = function(req, res){
-    // get screenshot files
     s3.listObjects({Bucket: 'aws-frontend-store', Prefix: 'CODE/screenshots'}, function(err, data) {
-        var urls = _.unique(data.Contents.map(function(content) {
-            return content.Key.split('/').pop().split('-').shift();
+        var pages = _.unique(data.Contents.map(function(content) {
+            return content.Key.split('/')[2];
         }));
-        res.render('index', { urls: urls });
+        res.render('index', { pages: pages });
     });
 };
 
 exports.page = function(req, res){
-    // get screenshot files
-    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: 'CODE/screenshots'}, function(err, data) {
-        var urls = _.unique(data.Contents.map(function(content) {
-            return content.Key.split('/').pop().split('-').shift();
+    var page = req.query.page,
+        breakpoint = req.query.breakpoint || 'mobile';
+    // get screenshot
+    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: 'CODE/screenshots/' + page + '/' + breakpoint}, function(err, data) {
+        var screenshots = _.unique(data.Contents.map(function(content) {
+            return 'https://s3-eu-west-1.amazonaws.com/' + data.Name + '/' + content.Key
         }));
-        res.render('page', { page: req.query.url });
+        res.render('page', { page: page, screenshots: screenshots });
     });
 };
