45220aedf6cbb56d0875348e449eedce7611935f
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-11 15:21:37 +0000

    pulling breakpoints from s3

--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,53 @@
 
 var AWS = require('aws-sdk'),
     _   = require('lodash'),
-    s3  = new AWS.S3();
+    s3  = new AWS.S3(),
+    env = 'CODE';
 
 exports.index = function(req, res){
-    var env = req.query.env || 'code';
-    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env.toUpperCase() + '/screenshots'}, function(err, data) {
-        var pages = data.Contents
+    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/', Delimiter: '/'}, function(err, data) {
+        var pages = data.CommonPrefixes
             // pull out page url (3rd part of the key)
-            .map(function(content) {
-                return content.Key.split('/')[2];
+            .map(function(prefix) {
+                return prefix.Prefix.split('/')[2];
             });
-        res.render('index', { pages: _.unique(pages) });
+        res.render('index', { pages: pages });
     });
 };
 
 exports.page = function(req, res){
     var page = req.query.page,
-        env = req.query.env || 'code',
         breakpoint = req.query.breakpoint || 'mobile';
-    // get screenshots for this page, breakpoint and env
-    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env.toUpperCase() + '/screenshots/' + page + '/' + breakpoint}, function(err, data) {
-        var screenshots = data.Contents
-            // order by date
-            .sort(function(screenshotOne, screenshotTwo) {
-                return screenshotTwo.LastModified - screenshotOne.LastModified;
-            })
-            // pull out src and date
-            .map(function(content) {
-                return {
-                    date: content.Key.split('/').slice(4, 8).join('/').split('.').shift(),
-                    src: 'https://' + data.Name + '.s3.amazonaws.com/' + content.Key
-                }
+    s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/', Delimiter: '/'}, function(err, data) {
+        var pages = data.CommonPrefixes
+            // pull out page url (3rd part of the key)
+            .map(function(prefix) {
+                return prefix.Prefix.split('/')[2];
             });
-        res.render('page', { page: page, breakpoint: breakpoint, screenshots: screenshots });
+        // get screenshots for this page, breakpoint and env
+        s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/' + page + '/', Delimiter: '/'}, function(err, data) {
+            var breakpoints = data.CommonPrefixes
+                // pull out breakpoint (4th part of the key)
+                .map(function(prefix) {
+                    return prefix.Prefix.split('/')[3];
+                });
+            // get screenshots for this page, breakpoint and env
+            s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/' + page + '/' + breakpoint}, function(err, data) {
+                var screenshots = data.Contents
+                    // order by date
+                    .sort(function(screenshotOne, screenshotTwo) {
+                        return screenshotTwo.LastModified - screenshotOne.LastModified;
+                    })
+                    // pull out src and date
+                    .map(function(content) {
+                        return {
+                            date: content.Key.split('/').slice(4, 8).join('/').split('.').shift(),
+                            src: 'https://' + data.Name + '.s3.amazonaws.com/' + content.Key
+                        }
+                    })
+                    .slice(0, 5);;
+                res.render('page', { page: page, breakpoint: breakpoint, screenshots: screenshots, pages: pages, breakpoints: breakpoints });
+            });
+        });
     });
 };
