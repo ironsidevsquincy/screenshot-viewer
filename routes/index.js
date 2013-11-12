33cbdb7f20e0903e22755fe1d7e4c2ca48c59943
Darren Hurley <darren.hurley@guardian.co.uk>
2013-11-12 17:00:24 +0000

    order of breakpoints

--- routes/index.js
+++ routes/index.js
@@ -22,18 +22,23 @@ exports.index = function(req, res){
 exports.page = function(req, res){
     var page = req.query.page,
         breakpoint = req.query.breakpoint || 'mobile';
+    // get the available pages
     s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/', Delimiter: '/'}, function(err, data) {
         var pages = data.CommonPrefixes
             // pull out page url (3rd part of the key)
             .map(function(prefix) {
                 return prefix.Prefix.split('/')[2];
             });
-        // get screenshots for this page, breakpoint and env
+        // get the available breakpoints
         s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/' + page + '/', Delimiter: '/'}, function(err, data) {
             var breakpoints = data.CommonPrefixes
                 // pull out breakpoint (4th part of the key)
                 .map(function(prefix) {
                     return prefix.Prefix.split('/')[3];
+                })
+                .sort(function(a, b) {
+                    var sizes = ['mobile', 'tablet', 'desktop', 'wide'];
+                    return sizes.indexOf(a) - sizes.indexOf(b);
                 });
             // get screenshots for this page, breakpoint and env
             s3.listObjects({Bucket: 'aws-frontend-store', Prefix: env + '/screenshots/' + page + '/' + breakpoint}, function(err, data) {
@@ -49,7 +54,8 @@ exports.page = function(req, res){
                             src: 'https://' + data.Name + '.s3.amazonaws.com/' + content.Key
                         }
                     })
-                    .slice(0, 5);;
+                    // take first 5
+                    .slice(0, 5);
                 res.render('page', { page: page, breakpoint: breakpoint, screenshots: screenshots, pages: pages, breakpoints: breakpoints });
             });
         });
